3cfaf3537a5d1c10bf5c1b8accb661f453e95f7e
Your Name <your.email@example.com>
2025-11-01 16:31:35 +0530

    updated repo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -80,7 +80,7 @@ const Intro = () => {
             <SubBox>
                 <Text>
                     <h1>Hi,</h1>
-                    <h3>I'm Dayanithi.</h3>
+                    <h3>I'm Day</h3>
                     <h6>I design and Code simple yet beautiful websites.</h6>
                 </Text>
             </SubBox>
