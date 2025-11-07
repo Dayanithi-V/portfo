fd75c16e42f1dd9b05df18c629eadf3bac0659f5
Your Name <your.email@example.com>
2025-11-07 14:46:02 +0530

    now

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -81,7 +81,7 @@ const Intro = () => {
                 <Text>
                     <h1>Hi,</h1>
                     <h3>I'm Dayanithi!</h3>
-                    <h6>I design and Code simple yet beautiful websites.</h6>
+                    <h6>I'm a full‑stack developer and AI engineer.</h6>
                 </Text>
             </SubBox>
             <SubBox>
